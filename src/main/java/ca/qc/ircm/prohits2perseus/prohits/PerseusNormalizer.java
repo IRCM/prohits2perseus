de7049fc9364359c32757198459ada5db37f41f6
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-02-07 19:22:17 -0500

    Splits normalize method.

--- src/main/java/ca/qc/ircm/prohits2perseus/prohits/PerseusNormalizer.java
+++ src/main/java/ca/qc/ircm/prohits2perseus/prohits/PerseusNormalizer.java
@@ -26,7 +26,14 @@ public class PerseusNormalizer {
    */
   public List<List<String>> normalize(List<List<String>> data,
       NormalizeMetadata normalizeMetadata) {
-    // Remove ignored samples.
+    List<List<String>> output = removeIgnoredSamples(data, normalizeMetadata);
+    normalizeData(output, normalizeMetadata);
+    replaceZeros(output, normalizeMetadata);
+    return output;
+  }
+
+  private List<List<String>> removeIgnoredSamples(List<List<String>> data,
+      NormalizeMetadata normalizeMetadata) {
     List<Integer> removeIndexes =
         IntStream.range(normalizeMetadata.samplesStartIndex, data.get(0).size())
             .filter(index -> normalizeMetadata.ignoreSamples.contains(data.get(0).get(index)))
@@ -35,15 +42,19 @@ public class PerseusNormalizer {
         .map(line -> IntStream.range(0, line.size()).filter(index -> !removeIndexes.contains(index))
             .mapToObj(index -> line.get(index)).collect(Collectors.toCollection(ArrayList::new)))
         .collect(Collectors.toCollection(ArrayList::new));
-    int geneLineNumber = IntStream.range(1, output.size()).filter(
-        i -> output.get(i).get(normalizeMetadata.geneNameIndex).equals(normalizeMetadata.geneName))
+    return output;
+  }
+
+  private void normalizeData(List<List<String>> data, NormalizeMetadata normalizeMetadata) {
+    int geneLineNumber = IntStream.range(1, data.size()).filter(
+        i -> data.get(i).get(normalizeMetadata.geneNameIndex).equals(normalizeMetadata.geneName))
         .findFirst().orElse(-1);
     if (geneLineNumber == -1) {
       throw new IllegalArgumentException(
           "gene " + normalizeMetadata.geneName + " not found in data");
     }
-    for (int i = normalizeMetadata.samplesStartIndex; i < output.get(geneLineNumber).size(); i++) {
-      String value = output.get(geneLineNumber).get(i);
+    for (int i = normalizeMetadata.samplesStartIndex; i < data.get(geneLineNumber).size(); i++) {
+      String value = data.get(geneLineNumber).get(i);
       try {
         Double.parseDouble(value);
       } catch (NumberFormatException e) {
@@ -51,13 +62,12 @@ public class PerseusNormalizer {
       }
     }
     List<Double> geneValues =
-        output.get(geneLineNumber).stream().skip(normalizeMetadata.samplesStartIndex)
+        data.get(geneLineNumber).stream().skip(normalizeMetadata.samplesStartIndex)
             .map(value -> Double.parseDouble(value)).collect(Collectors.toList());
     double max = geneValues.stream().mapToDouble(v -> v).max().orElse(1.0);
     List<Double> multiplier =
         geneValues.stream().map(value -> max / value).collect(Collectors.toList());
-    // Normalize.
-    output.stream().skip(1).forEach(line -> {
+    data.stream().skip(1).forEach(line -> {
       IntStream.range(normalizeMetadata.samplesStartIndex, line.size()).forEach(index -> {
         String rawvalue = line.get(index);
         Double value = rawvalue.isEmpty() ? 0.0 : Double.parseDouble(rawvalue);
@@ -65,21 +75,22 @@ public class PerseusNormalizer {
             String.valueOf(value * multiplier.get(index - normalizeMetadata.samplesStartIndex)));
       });
     });
-    // Replace zeros.
-    List<Double> mins = IntStream.range(normalizeMetadata.samplesStartIndex, output.get(0).size())
+  }
+
+  private void replaceZeros(List<List<String>> data, NormalizeMetadata normalizeMetadata) {
+    List<Double> mins = IntStream.range(normalizeMetadata.samplesStartIndex, data.get(0).size())
         .mapToDouble(index -> {
           List<String> rawvalues =
-              output.stream().skip(1).map(line -> line.get(index)).collect(Collectors.toList());
+              data.stream().skip(1).map(line -> line.get(index)).collect(Collectors.toList());
           return rawvalues.stream().filter(value -> !value.equals("0.0"))
               .mapToDouble(value -> Double.parseDouble(value)).min().orElse(0.0);
         }).map(value -> value * 0.9).boxed().collect(Collectors.toList());
-    output.stream().skip(1).forEach(line -> {
+    data.stream().skip(1).forEach(line -> {
       IntStream.range(normalizeMetadata.samplesStartIndex, line.size()).forEach(index -> {
         if (line.get(index).equals("0.0")) {
           line.set(index, String.valueOf(mins.get(index - normalizeMetadata.samplesStartIndex)));
         }
       });
     });
-    return output;
   }
 }
